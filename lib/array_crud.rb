04765687428a56fa701acd82db8bc72c056a7c18
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-01-12 08:45:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  mycolors = ["green", "red", "blue", "gray"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  mycolors << ["white"]
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  mycolors.unshift = ["purple"]
 end
 
 def remove_element_from_end_of_array(array)
-  
+  mycolors.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  mycolors.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  mycolors.index_number[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  mycolors.[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  mycolors.[-1]
 end
