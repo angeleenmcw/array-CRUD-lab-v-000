9e6e28e60b5ba6f5fb3304998cd760b1d4e3b6cb
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-01-12 08:48:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,10 +18,12 @@ end
 
 def remove_element_from_end_of_array(array)
   mycolors.pop
+  puts mycolors.inspect
 end
 
 def remove_element_from_start_of_array(array)
   mycolors.shift
+  puts mycolors.inspect
 end
 
 def retrieve_element_from_index(array, index_number)
